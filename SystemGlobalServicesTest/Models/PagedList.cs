3c3f53047f7cf3e22a910666bf2c634cce156d73
Vinni949 <pumba0@yandex.ru>
2023-03-04 10:21:32 +0300

    AddClassPagedList

---
+++ SystemGlobalServicesTest/Models/PagedList.cs
@@ -0,0 +1,18 @@
+﻿namespace SystemGlobalServicesTest.Models
+{
+    public class PagedList<T> : List<T>
+    {
+        public int CurrentPage { get; set; }
+        public int TotalCount { get; set; }
+        public PagedList(int currentPage, int totalItems, List<T> items, int pageSize)
+        {
+            CurrentPage = currentPage;
+            this.AddRange(items);
+            TotalCount = (int)(Math.Ceiling(totalItems / (double)(pageSize)));
+
+        }
+
+        public bool HasNextPage() => CurrentPage < TotalCount;
+        public bool HasPreviousPage() => CurrentPage > 0;
+    }
+}
